2fbd412662aa2938a0e04802190bad3c6ef69471
Richard Woods <Richard@Richards-MacBook-Pro.local>
2020-09-05 12:55:42 -0500

    initialize

---
+++ core/include/core.h
@@ -0,0 +1,4 @@
+
+inline const char* getHelloWorld() {
+    return "Goodbye World\n";
+}
---
+++ core/src/core.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+#include "core.h"
+
+int main() {
+    printf(getHelloWorld());
+    return 0;
+}
